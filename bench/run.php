e2d74869ea26235dcc2fcfeabc8fe372bd3a2eb0
Andreas Hennings <andreas@dqxtech.net>
2016-06-27 01:50:17 +0200

    Add backdrop sort equivalent.

--- bench/run.php
+++ bench/run.php
@@ -20,8 +20,12 @@ for ($i = 0; $i < 1000; ++$i) {
 $x0 = SortArrays::sortByWeightKey_itemsByWeight($items_unsorted, 'weight', SORT_NUMERIC);
 $x1 = SortArrays::sortByWeightKey_keysByWeight($items_unsorted, 'weight', SORT_NUMERIC);
 $x2 = SortArrays::sortByWeightKey_weightWithFraction($items_unsorted, 'weight', SORT_NUMERIC);
+$x3 = SortArrays::sortByWeightKey_backdrop($items_unsorted, 'weight', SORT_NUMERIC);
 assert($x0 === $x1);
 assert($x0 === $x2);
+// This is not the same, because the backdrop sort is not stable.
+# assert($x0 === $x3);
+assert($x0 == $x3);
 
 $dts = [];
 $t0 = microtime(true);
@@ -44,6 +48,12 @@ for ($j = 0; $j < 4; ++$j) {
   }
 
   $t0 = ($dts['weightWithFraction'][] = microtime(true) - $t0) + $t0;
+
+  for ($i = 0; $i < 1000; ++$i) {
+    $items_sorted = SortArrays::sortByWeightKey_backdrop($items_unsorted, 'weight', SORT_NUMERIC);
+  }
+
+  $t0 = ($dts['backdrop'][] = microtime(true) - $t0) + $t0;
 }
 
 print_r($dts);
--- src/SortArrays.php
+++ src/SortArrays.php
@@ -95,4 +95,118 @@ final class SortArrays extends UtilBase {
     return $items_sorted;
   }
 
+  /**
+   * @param array[] $items_unsorted
+   * @param string|int $weight_key
+   * @param int $sort_flags
+   *
+   * @return array[]
+   */
+  public static function sortByWeightKey_backdrop(array $items_unsorted, $weight_key, $sort_flags = null) {
+    self::backdrop_sort($items_unsorted, array('weight' => $sort_flags));
+    return $items_unsorted;
+  }
+
+    /**
+     * Sort an array based on user-provided keys within that array.
+     *
+     * @param array $array
+     *   The input array to be sorted.
+     * @param array $keys
+     *   An array of keys on which to be sort. Each key of the $keys array is the
+     *   key by which sorting will be done. Each value of the $keys array must be
+     *   one of the PHP constants sorting, either SORT_NUMERIC or SORT_STRING.
+     *   If an unindexed array is passed in, each value will be assumed to be the
+     *   key by which sorting will be done, and all comparisons will be done as
+     *   though SORT_NUMERIC were passed in for each key.
+     */
+  private static function backdrop_sort(array &$array, array $keys = array('weight')) {
+    // Ensure all keys have a sort value.
+    $new_keys = array();
+    foreach ($keys as $index => $sort) {
+      if (is_string($sort) && is_int($index)) {
+        $new_keys[$sort] = SORT_NUMERIC;
+      }
+      else {
+        if ($sort === SORT_NUMERIC || $sort === SORT_STRING) {
+          $new_keys[$index] = $sort;
+        }
+        else {
+          // Fallback behavior for unexpected values. Untranslated to allow this
+          // function to be used anywhere within Backdrop.
+          $new_keys[$index] = SORT_NUMERIC;
+          trigger_error('backdrop_sort() expects the second parameter to be an array keyed by strings and each value of the array to be either SORT_NUMERIC or SORT_STRING.', E_USER_WARNING);
+        }
+      }
+    }
+    $keys = $new_keys;
+    // If sorting on a single value, optimize the sorting to sort only on that
+    // specific key without the overhead of recursive handling.
+    if (count($keys) === 1) {
+      $key = key($keys);
+      $key_sort = reset($keys);
+      if ($key_sort === SORT_STRING) {
+        uasort($array, function($a, $b) use ($key) {
+          if (!is_array($a) || !is_array($b)) {
+            return 0;
+          }
+          if (!isset($a[$key])) {
+            $a[$key] = '';
+          }
+          if (!isset($b[$key])) {
+            $b[$key] = '';
+          }
+          return strnatcasecmp($a[$key], $b[$key]);
+        });
+      }
+      else {
+        uasort($array, function($a, $b) use ($key) {
+          if (!is_array($a) || !is_array($b)) {
+            return 0;
+          }
+          if (!isset($a[$key])) {
+            $a[$key] = 0;
+          }
+          if (!isset($b[$key])) {
+            $b[$key] = 0;
+          }
+          if ($a[$key] != $b[$key]) {
+            return $a[$key] < $b[$key] ? -1 : 1;
+          }
+        });
+      }
+    }
+    // If doing a multiple-key comparison, use a recursive callback.
+    else {
+      $keys_map = array_keys($keys);
+      $recursive_callback = function ($a, $b, $key_index = 0) use ($keys, $keys_map, &$recursive_callback) {
+        $key = $keys_map[$key_index];
+        if (!is_array($a) || !is_array($b)) {
+          return 0;
+        }
+        if (!isset($a[$key])) {
+          $a[$key] = 0;
+        }
+        if (!isset($b[$key])) {
+          $b[$key] = 0;
+        }
+        if ($a[$key] == $b[$key]) {
+          // Sort on the next key if one exists.
+          if (isset($keys_map[$key_index + 1])) {
+            return $recursive_callback($a, $b, $key_index + 1);
+          }
+        }
+        else {
+          if ($keys[$key] === SORT_STRING) {
+            return strnatcasecmp($a[$key], $b[$key]);
+          }
+          else {
+            return ($a[$key] < $b[$key]) ? -1 : 1;
+          }
+        }
+      };
+      uasort($array, $recursive_callback);
+    }
+  }
+
 }
