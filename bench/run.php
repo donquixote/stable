1304b9cc51e228af40c318c5b8713fa814c61493
Andreas Hennings <andreas@dqxtech.net>
2016-06-27 03:08:02 +0200

    Remove backdrop sort from benchmark.

--- bench/run.php
+++ bench/run.php
@@ -20,9 +20,11 @@ for ($i = 0; $i < 1000; ++$i) {
 $x0 = SortArrays::sortByWeightKey_itemsByWeight($items_unsorted, 'weight', SORT_NUMERIC);
 $x1 = SortArrays::sortByWeightKey_keysByWeight($items_unsorted, 'weight', SORT_NUMERIC);
 $x2 = SortArrays::sortByWeightKey_weightWithFraction($items_unsorted, 'weight', SORT_NUMERIC);
+$xm = SortArrays::sortByWeightKey_arrayMultisort($items_unsorted, 'weight', SORT_NUMERIC);
 $x3 = SortArrays::sortByWeightKey_backdrop($items_unsorted, 'weight', SORT_NUMERIC);
 assert($x0 === $x1);
 assert($x0 === $x2);
+assert($x0 === $xm);
 // This is not the same, because the backdrop sort is not stable.
 # assert($x0 === $x3);
 assert($x0 == $x3);
@@ -50,10 +52,18 @@ for ($j = 0; $j < 4; ++$j) {
   $t0 = ($dts['weightWithFraction'][] = microtime(true) - $t0) + $t0;
 
   for ($i = 0; $i < 1000; ++$i) {
+    $items_sorted = SortArrays::sortByWeightKey_arrayMultisort($items_unsorted, 'weight', SORT_NUMERIC);
+  }
+
+  $t0 = ($dts['arrayMultisort'][] = microtime(true) - $t0) + $t0;
+
+  /*
+  for ($i = 0; $i < 1000; ++$i) {
     $items_sorted = SortArrays::sortByWeightKey_backdrop($items_unsorted, 'weight', SORT_NUMERIC);
   }
 
   $t0 = ($dts['backdrop'][] = microtime(true) - $t0) + $t0;
+  */
 }
 
 print_r($dts);
